5e66d0612291124636f1e5deb380f0b23265a1b0
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-24 18:40:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :s_one, :s_two, :s_three
+  
+  def initialize(s_one, s_two, s_three)
+    # if (s_one + s_two < s_three) || (s_two + s_three < s_one) || (s_three + s_one > s_two) || s_one <= 0 || s_two <= 0 || s_three < 0 
+    #   begin 
+    #     raise TriangleError
+    #   rescue TriangleError => error 
+    #       puts error.message
+    #   end
+    # else 
+      @s_one = s_one
+      @s_two = s_two
+      @s_three = s_three
+    # end
+    
+  end 
+  
+  def kind 
+    if (s_one + s_two < s_three) || (s_two + s_three < s_one) || (s_three + s_one < s_two) || s_one <= 0 || s_two <= 0 || s_three <= 0 
+      begin 
+        raise TriangleError
+      rescue TriangleError => error 
+          puts error.message
+      end
+    
+    elsif s_one == s_two && s_two == s_three 
+      :equilateral 
+    elsif s_one == s_two || s_two == s_three || s_three == s_one
+      :isosceles
+    else
+      :scalene
+    end 
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      "You must enter side lengths equal to a valid triangle"
+    end 
+  end 
+  
 end
