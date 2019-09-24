0614ad899facef8cd531bd1cfe38ff2ada84af15
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-24 18:57:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
-end
+  
+  attr_reader :s_one, :s_two, :s_three
+  
+  def initialize(s_one, s_two, s_three)
+      @s_one = s_one
+      @s_two = s_two
+      @s_three = s_three
+  end 
+  
+  def kind 
+    if (@s_one + @s_two <= @s_three) || (@s_two + @s_three <= @s_one) || (@s_three + @s_one <= @s_two) || @s_one <= 0 || @s_two <= 0 || @s_three <= 0
+      raise TriangleError    
+    
+    elsif @s_one == @s_two && @s_two == @s_three 
+      :equilateral 
+    elsif @s_one == @s_two || @s_two == @s_three || @s_three == @s_one
+      :isosceles
+    else
+      :scalene
+    end 
+  end 
+  
+
+
+  class TriangleError < StandardError
+  end 
+end 
