9ba5d21c1bd2a7ca1c9018d1b360426c89bbea06
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-24 18:55:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if (@s_one + @s_two <= @s_three) || (@s_two + @s_three <= @s_one) || (@s_three + @s_one <= @s_two) || @s_one <= 0 || @s_two <= 0 || @s_three <= 0
       raise TriangleError    
     
-    elsfif @s_one == @s_two && @s_two == @s_three 
+    elsif @s_one == @s_two && @s_two == @s_three 
       :equilateral 
     elsif @s_one == @s_two || @s_two == @s_three || @s_three == @s_one
       :isosceles
