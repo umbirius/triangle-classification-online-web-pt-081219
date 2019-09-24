204a0e2b15fb9ef422226e6827d3ec6f10af4df8
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-24 18:53:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,10 @@ class Triangle
   end 
   
   def kind 
-    if s_one == s_two && s_two == s_three 
+    if (s_one + s_two < s_three) || (s_two + s_three < s_one) || (s_three + s_one < s_two) || s_one <= 0 || s_two <= 0 || s_three <= 0
+      raise TriangleError    
+    
+    elsifif s_one == s_two && s_two == s_three 
       :equilateral 
     elsif s_one == s_two || s_two == s_three || s_three == s_one
       :isosceles
